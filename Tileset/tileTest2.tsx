dcee455f31467cb438a505a45fb813d289bb6cfb
Huzaifa <huzaifakhan3339@gmail.com>
2024-07-09 15:39:39 -0700

    tiled_testing stuff
    Plus world map

---
+++ Tileset/tileTest2.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileTest2" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../../../../Downloads/tileTest2.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="10" width="8.54545" height="8.54545"/>
+   <object id="2" x="0" y="0" width="31.9091" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="31.9091"/>
+   <object id="2" x="-1.36364" y="-29"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0909091" y="0" width="31.9091" height="31.6364"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32.1818" height="31.9091"/>
+  </objectgroup>
+ </tile>
+</tileset>
