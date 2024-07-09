926e7a6ecc7e04c929cd4d3640216b9eaa69560f
Eliot Bitting <55938544+ElBitting@users.noreply.github.com>
2024-07-08 21:34:20 -0500

    Update player.lua
    fix extra large jump bug i think. also added visual for grounded detection.

--- player.lua
+++ player.lua
@@ -1,4 +1,4 @@
-player = world:newCircleCollider(400, 100, 16)
+player = world:newCircleCollider(400, 100, 18)
 player:setCollisionClass("Player")
 
 image = love.graphics.newImage('Sprites/character/oldHero.png')
@@ -10,15 +10,17 @@ player.anim = animation
 player.ismoving = False
 player.dir = 1
 player.grounded = 'Ugggga'
+colliderWidth = 20
+colliderHeight = 18
 
 function player:update(dt)
     -- Reset Horizontal velocity
     xNow,yNow = player:getLinearVelocity()
-	player:setLinearVelocity(0, yNow) 
+	player:setLinearVelocity(0, yNow)
 
     -- Check if grounded
     if player.body then
-        local colliders = world:queryRectangleArea(player:getX() - 10, player:getY() +2, 40, 15, {'Platform'})
+        local colliders = world:queryRectangleArea(player:getX()-colliderWidth/2, player:getY()+3, colliderWidth, colliderHeight, {'Platform'})
         if #colliders > 0 then
             player.grounded = 'True'
         else
@@ -43,6 +45,7 @@ function player:update(dt)
             player:applyLinearImpulse(0,-500)
         end
     end
+        
 
     --States For Animations (once we have them)
     local Currentrunning = player.ismoving and player.grounded
@@ -57,12 +60,16 @@ function player:draw()
     local px = player:getX()
     local py = player:getY()
 
+    --scale character by 3
     sx = 3
     sy = 3
+    --swap direction for facing left vs right
     if player.dir == -1 then
         sx = -sx
     end
 
-    player.anim:draw(image, px, py-30, nil, sx, sy)
+    player.anim:draw(image, px-(20*player.dir), py-30, nil, sx, sy)
+    -- show grounded detection
     love.graphics.print(player.grounded, 10, 10)
+    love.graphics.rectangle('line',px-colliderWidth/2, py+3, colliderWidth, colliderHeight)
 end
