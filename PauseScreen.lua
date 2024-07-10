c337a8460471a95aea28677e1f41d360f86f1df5
Eliot Bitting <55938544+ElBitting@users.noreply.github.com>
2024-07-09 19:29:47 -0500

    Move PauseMenu Logic
    he he

---
+++ PauseScreen.lua
@@ -0,0 +1,45 @@
+PauseScreen = {}
+buttons = {}
+
+function newButton(text, fn)
+    return { 
+        text = text,
+        fn = fn,
+        now = false,
+        last = false
+    }
+end
+
+table.insert(buttons, newButton("Resume", function() pause = false end))
+table.insert(buttons, newButton("Inventory", function() end))
+table.insert(buttons, newButton("Save", function() pause = false end))
+table.insert(buttons, newButton("Settings", function() end))
+table.insert(buttons, newButton("Exit", function() love.event.quit() end))
+
+function PauseScreen: draw()
+     screenWidth = love.graphics.getWidth()
+     screenHeight = love.graphics.getHeight()
+     butt = 0 
+    if pause == true then
+        love.graphics.print('Paused', gameFont, 325,100)
+        love.graphics.rectangle("line", screenWidth-menuWidth-10, screenHeight /9, menuWidth, menuHeight)
+        for i, button in ipairs(buttons) do
+            button.last = button.now
+            local color = {0.5, 0.7, 0.8, 1}
+            local bx, by = screenWidth-menuWidth+20, screenHeight / 9 + 20 + butt * 50 
+            local mx, my = love.mouse.getPosition()
+            local hot = (mx > bx and mx < bx + menuWidth) and (my >by and my < by +50) 
+            if hot then
+                color = {0.7, 0.7, 0.9}
+            end
+            button.now = love.mouse.isDown(1)
+            if (button.now or love.keyboard.isDown('return')) and not button.last and hot then
+                button.fn()
+            end
+            love.graphics.setColor(unpack(color))
+            love.graphics.print(button.text, textFont,bx,by )
+            butt = butt +1
+        end
+        love.graphics.setColor(1,1,1)
+    end
+end
--- main.lua
+++ main.lua
@@ -2,9 +2,6 @@ anim8 = require 'library/anim8'
 wf = require 'library/windfield'
 camera = require 'library/hump-master/camera'
 
-
-local buttons = {}
-
 function love.load()
     love.window.setMode(1000, 768)
     gameFont = love.graphics.newFont(100)
@@ -18,11 +15,7 @@ function love.load()
     ground:setCollisionClass('Platform')
     ground:setType('static') 
     require 'player'
-    table.insert(buttons, newButton("Resume", function() pause = false end))
-    table.insert(buttons, newButton("Inventory", function() end))
-    table.insert(buttons, newButton("Save", function() pause = false end))
-    table.insert(buttons, newButton("Settings", function() end))
-    table.insert(buttons, newButton("Exit", function() love.event.quit() end))
+    require 'PauseScreen'
     menuHeight = love.graphics.getHeight() *3/5
     menuWidth = love.graphics.getWidth() /5
 end
@@ -40,32 +33,8 @@ function love.draw()
         world:draw()
         player:draw()
     cam:detach()
-
-    local screenWidth = love.graphics.getWidth()
-    local screenHeight = love.graphics.getHeight()
-    local butt = 0 
-    if pause == true then
-        love.graphics.print('Paused', gameFont, 325,100)
-        love.graphics.rectangle("line", screenWidth-menuWidth-10, screenHeight /9, menuWidth, menuHeight)
-        for i, button in ipairs(buttons) do
-            button.last = button.now
-            local color = {0.5, 0.7, 0.8, 1}
-            local bx, by = screenWidth-menuWidth+20, screenHeight / 9 + 20 + butt * 50 
-            local mx, my = love.mouse.getPosition()
-            local hot = (mx > bx and mx < bx + menuWidth) and (my >by and my < by +50) 
-            if hot then
-                color = {0.7, 0.7, 0.9}
-            end
-            button.now = love.mouse.isDown(1)
-            if button.now and not button.last and hot then
-                button.fn()
-            end
-            love.graphics.setColor(unpack(color))
-            love.graphics.print(button.text, textFont,bx,by )
-            butt = butt +1
-        end
-        love.graphics.setColor(1,1,1)
-    end
+    PauseScreen:draw()
+    
     love.graphics.print(player.grounded, textFont, 10, 10)
 end
 
@@ -73,16 +42,4 @@ function love.keypressed(key)
     if key == 'escape' then
         pause = not pause
     end
-    if pause and key == 'return' then
-        love.event.quit()
-    end
-end
-
-function newButton(text, fn)
-    return { 
-        text = text,
-        fn = fn,
-        now = false,
-        last = false
-    }
 end
