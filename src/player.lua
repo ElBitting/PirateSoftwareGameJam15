e6ff47193e58b8fb08d768d8869d5bc8859814f3
Eliot Bitting <55938544+ElBitting@users.noreply.github.com>
2024-07-21 17:51:29 -0500

    best movement

--- src/player.lua
+++ src/player.lua
@@ -9,18 +9,19 @@ Playergrid = anim8.newGrid(32,32, image: getWidth(), image:getHeight())
 animation = anim8.newAnimation(Playergrid('1-6',1), 0.15)
 
 player.speed = 130
+player.jumpspeed = 85
 player.anim = animation
 player.ismoving = false
-player.dir = 1
 player.grounded = true
 player.laddered = false
 player.x = 0
 player.y = 0
 player.inventory = {}
+
 -- player.hasapple = false
-local colliderWidth = 2
-local colliderHeight = 3
-local offsetCollionPlayerFeet = 5
+local colliderWidth = 6
+local colliderHeight = 2
+local offsetCollionPlayerFeet = 6
 
 function player:update(dt)
     -- Reset Horizontal velocity
@@ -28,58 +29,63 @@ function player:update(dt)
     player:setLinearVelocity(player.x * player.speed, yNow)
     -- player:setLinearVelocity(0, yNow)
 
-    player.x = 0
-
-
-    --Basic Movement
-    local wall = world:queryRectangleArea(player:getX()+5*player.dir, player:getY()-3, 2*player.dir, 6, {'ThickWalls'})
-    if love.keyboard.isDown('a') then
+    -- player movement (No friction necessary!)
+    if love.keyboard.isDown('a') and not love.keyboard.isDown('d') then 
         player.ismoving = true
-        player.dir = -1
-        if #wall == 0 then player.x = -1 end
-    elseif love.keyboard.isDown('d')  then
+        player.x = -1
+    elseif love.keyboard.isDown('d') and not love.keyboard.isDown('a') then 
         player.ismoving = true
-        player.dir = 1
-        if #wall == 0 then player.x = 1 end
+        player.x = 1
+    else
+        player.ismoving = false
+        player.x = 0
     end
+
+
+    --Wall detection
+    local wall = world:queryRectangleArea(player:getX()+5*player.x, player:getY()-3, 2*player.x, 6, {'ThickWalls'})
+    if #wall > 0 then
+        player.x = 0
+    end
+
 
 
     --Ladder and Jumping
-    -- Check if grounded
-    local Jump1 = Timer.after(0.1, function() player.grounded = true end)
-    local Jump2 = Timer.after(0.1, function() player.laddered = true end)
 
+    
+    --Grounding
     local colliders = world:queryRectangleArea(player:getX()-colliderWidth/2, player:getY()+offsetCollionPlayerFeet, colliderWidth, colliderHeight, {'Platform', 'ThickWalls'})
     if #colliders > 0 then
-        Jump1 = Timer.after(0.1, function() player.grounded = true end)
+        player.grounded = true
     else
-        Timer.cancel(Jump1)
         player.grounded = false
     end
+
+
     local ladder = world:queryRectangleArea(player:getX()-6, player:getY()+3, 12, 4, {'Ladders'})
     if #ladder > 0  then
+        --gravity on ladder
         if yNow < 0 then
             player:setGravityScale(1)
         else 
             player:setLinearVelocity(player.x * player.speed, 0)
             player:setGravityScale(0)
         end
+
+        --climbing and descending ladder
         if love.keyboard.isDown('w') then
             player:setY(player:getY() - player.speed*dt)
         elseif love.keyboard.isDown('s') then
             player:setY(player:getY() + player.speed*dt)
         end
-        Jump2 = Timer.after(0.1, function() player.laddered = true end)
+
+        --set state
+        player.laddered = true
     else
         player:setGravityScale(1)
-        Timer.cancel(Jump2)
         player.laddered = false
     end
-    Timer.update(dt)
 
-
-
-    -- Jumping
     
 
     
@@ -139,30 +145,24 @@ function player:draw()
     local sx = .8
     local sy = .8
     --swap direction for facing left vs right
-    if player.dir == -1 then
+    if player.x == -1 then
         sx = -sx
     end
-
-    
-    -- show grounded detection
-    -- love.graphics.rectangle('line',px-colliderWidth/2, py+offsetCollionPlayerFeet, colliderWidth, colliderHeight)
-    -- love.graphics.rectangle('line', px+(4*player.dir), py-2.5,2*player.dir, 5)
 
     player.anim:draw(image, px, py, nil, sx, sy,16, 24)
 end
 
 function player:keypressed(key)
-    if key == 'space' and player.laddered then
+    -- jump logic
+    if key == 'space' and (player.grounded or player.laddered) then
         player:setLinearVelocity(0, 0)
-        player:applyLinearImpulse(0,-52)
-    elseif key == 'space' and player.grounded then
-        -- jump impulse
-        player:setLinearVelocity(0, 0)
-        player:applyLinearImpulse(0,-85)
+        player:applyLinearImpulse(0,-player.jumpspeed)
     end
+
 end
 
 function player:keyrealeased(key)
+    -- jump logic
     if key == 'space' then
         local X, Y = player:getLinearVelocity()
         if Y < 0 then
