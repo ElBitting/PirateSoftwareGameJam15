dda2b985324340b4f555c6d175121ba5dee332cd
Huzaifa <huzaifakhan3339@gmail.com>
2024-07-22 18:06:54 -0700

    Small fix to credit for controller support

--- src/Menus/Credits.lua
+++ src/Menus/Credits.lua
@@ -12,7 +12,7 @@ function Credits:keypressed(key, gamepad)
     if key == 'escape' or (gamepad and key == 'b') then 
         safe_quit()
     end
-    if key == 'return' then 
+    if key == 'return' or (gamepad and key == 'a')  then 
         safe_restart()
     end
 end
