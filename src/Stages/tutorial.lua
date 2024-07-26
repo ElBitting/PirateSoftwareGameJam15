5da9c4c2dba9d560f09c34d43f0611e7402270b7
ghkkhan <huzaifakhan3339@gmail.com>
2024-07-25 22:58:34 -0500

    Vine disappearance after first reset fixed

--- src/Stages/tutorial.lua
+++ src/Stages/tutorial.lua
@@ -22,6 +22,7 @@ function tutorial:enter()
     add_col_class_obj(ladders, 'Ladders','Ladders', true, false)
     add_col_class_obj(twals, 'ThickWalls','ThickWalls', true, false)
     add_col_class_obj(vines, 'Vines','Vines', false, true)
+    vines_destroyed = false
 
     add_entities()
 
