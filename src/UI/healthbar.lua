c0067ffef93631a2826104b74a6225925e0e1a36
Eliot Bitting <55938544+ElBitting@users.noreply.github.com>
2024-07-26 16:17:12 -0500

    less magic numbers and better scaling on health

--- src/UI/healthbar.lua
+++ src/UI/healthbar.lua
@@ -6,19 +6,20 @@ healthbar.fullHealth = love.graphics.newImage('Art/Sprites/Vial.png')
 healthbar.halfHealth = love.graphics.newImage('Art/Sprites/Vial_kracked.png')
 
 function healthbar:draw()
+    local sx, sy = 3, 3
     if player.health >= 2 then
-        love.graphics.draw(healthbar.fullHealth, 0, 0, 0, 5,5, -5, -5)
+        love.graphics.draw(healthbar.fullHealth, 0, 0, 0, sx,sy, -5, -5)
         if player.health >= 4 then
-            love.graphics.draw(healthbar.fullHealth, 0, 0, 0, 5,5, -32-5, -5)
+            love.graphics.draw(healthbar.fullHealth, 0, 0, 0, sx,sy, -32-5, -5)
             if player.health >= 6 then
-                love.graphics.draw(healthbar.fullHealth, 0, 0, 0, 5,5, -64 - 5, -5)
+                love.graphics.draw(healthbar.fullHealth, 0, 0, 0, sx,sy, -64 - 5, -5)
             elseif player.health >= 5 then
-                love.graphics.draw(healthbar.halfHealth, 0, 0, 0, 5,5, -64 - 5, -5)
+                love.graphics.draw(healthbar.halfHealth, 0, 0, 0, sx,sy, -64 - 5, -5)
             end
         elseif player.health >= 3 then
-            love.graphics.draw(healthbar.halfHealth, 0, 0, 0, 5,5, -32-5, -5)
+            love.graphics.draw(healthbar.halfHealth, 0, 0, 0, sx,sy, -32-5, -5)
         end
     else
-        love.graphics.draw(healthbar.halfHealth, 0, 0, 0, 5,5, -5, -5)
+        love.graphics.draw(healthbar.halfHealth, 0, 0, 0, sx,sy, -5, -5)
     end
 end
