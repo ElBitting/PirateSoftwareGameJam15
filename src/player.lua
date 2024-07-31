59eb464cf07c9ee1d0b0a71f36992bcb378f9f7e
Eliot Bitting <55938544+ElBitting@users.noreply.github.com>
2024-07-30 20:40:21 -0500

    fixed timer pt2

--- src/player.lua
+++ src/player.lua
@@ -45,7 +45,7 @@ function player:update(dt)
     player:initializations(dt)
     if player.canMove then 
         player:movement(dt)
-        Timer.cancel(movementTimer)
+        Timer.clear(movementTimer)
     end
     player:detectections(dt)
     player:ladders(dt)
@@ -258,7 +258,7 @@ function player:interactions(dt)
         end)
     end
     if player:exit('Hazards') then
-        Timer.cancel(SpikeDMG)
+        Timer.clear(SpikeDMG)
     end
 
     if player:enter('Shadow') then 
